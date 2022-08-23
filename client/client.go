00db93abad0273eba22f424b93e234f1ff7d4438
Il Harper <hi@ilharper.com>
2022-08-23 16:44:20 +0000

    chore: publish koishijs/koishi-desktop@73c73fe2ecbcd0d440672824bdfb3a4391e4cf03

--- client/client.go
+++ client/client.go
@@ -1,7 +1,9 @@
+//nolint:wrapcheck
 package client
 
 import (
 	"fmt"
+	"net"
 
 	"golang.org/x/net/websocket"
 	"gopkg.ilharper.com/koi/core/god"
@@ -14,10 +16,10 @@ type Options struct {
 
 // Connect tries to connect to Koi god daemon
 // and returns a bare [websocket.Conn].
-func Connect(options *Options) (client *websocket.Conn, err error) {
+func Connect(options *Options) (*websocket.Conn, error) {
 	return websocket.Dial(
-		fmt.Sprintf("ws://%s:%s%s", options.Host, options.Port, god.DaemonEndpoint),
+		fmt.Sprintf("ws://%s%s", net.JoinHostPort(options.Host, options.Port), god.DaemonEndpoint),
 		"",
-		fmt.Sprintf("http://%s:%s/", options.Host, options.Port),
+		fmt.Sprintf("http://%s/", net.JoinHostPort(options.Host, options.Port)),
 	)
 }
